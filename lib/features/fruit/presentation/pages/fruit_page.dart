9c6fc316143264911cf34b1618456f36fe37cbdd
Dimitri LEURS <dimitri.leurs@devoteam.com>
2023-05-31 14:27:41 +0200

    feat(#3): Improving UI fruits screen

--- lib/features/fruit/presentation/pages/fruit_page.dart
+++ lib/features/fruit/presentation/pages/fruit_page.dart
@@ -1,6 +1,7 @@
 import 'package:dart_extensions/dart_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_starter/features/fruit/domain/entities/fruit.dart';
 import 'package:flutter_starter/features/fruit/presentation/cubit/fruit_cubit.dart';
 import 'package:flutter_starter/features/fruit/presentation/cubit/fruit_state.dart';
 import 'package:get_it/get_it.dart';
@@ -10,19 +11,32 @@ class FruitPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<FruitCubit>(
-      create: (context) => GetIt.instance.get<FruitCubit>()..getFruits(),
-      child: BlocBuilder<FruitCubit, FruitState>(
-        builder: (context, state) {
-          if (state.isLoading) {
-            return const CircularProgressIndicator();
-          } else if (state.errorMessage.isEmptyOrNull == false) {
-            return Text("Un error occured : ${state.errorMessage}");
-          }
-          return ListView.builder(
-              itemCount: state.fruits.length,
-              itemBuilder: (context, index) => Text(state.fruits[index].name));
-        },
+    return Scaffold(
+      body: SafeArea(
+        child: BlocProvider<FruitCubit>(
+          create: (context) => GetIt.instance.get<FruitCubit>()..getFruits(),
+          child: BlocBuilder<FruitCubit, FruitState>(
+            builder: (context, state) {
+              if (state.isLoading) {
+                return const CircularProgressIndicator();
+              } else if (state.errorMessage.isEmptyOrNull == false) {
+                return Text("Un error occured : ${state.errorMessage}");
+              }
+              return ListView.builder(
+                  itemCount: state.fruits.length,
+                  itemBuilder: (context, index) {
+                    final fruit = state.fruits[index];
+                    return ListTile(
+                      title: Text(fruit.name),
+                      subtitle: Text(fruit.family),
+                      trailing: fruit.genus == FruitGenus.citrus
+                          ? const Icon(Icons.restaurant_menu)
+                          : null,
+                    );
+                  });
+            },
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(body: FruitPage()),
+      home: const FruitPage(),
     );
   }
 }
