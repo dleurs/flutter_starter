a7576512bb93a99af21fe8df619b5d763e429a94
Dimitri LEURS <dimitri.leurs-ext@rexel.com>
2023-07-13 11:11:18 +0200

    chore(#noticket): reordering file structure

--- lib/core/code_injection/injection.dart
+++ lib/core/code_injection/injection.dart
@@ -1,6 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
-import 'package:flutter_starter/core/injection/injection.config.dart';
+import 'package:flutter_starter/core/code_injection/injection.config.dart';
 
 final getIt = GetIt.instance;
 
--- lib/theme/app_theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppTheme {
-  static final lightTheme = ThemeData(
-    brightness: Brightness.light,
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-    useMaterial3: true,
-  );
-}
